70a2c4b94b9cfb4d1cfd4bab6ee2d2f0bfa66c51
Mayen Akpan <neroxma@yahoo.com>
2021-07-17 08:00:56 -0700

    Update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import "./App.css";
 
 function App() {
+  const SEARCH_API_URL =
+    "https://[siteId].a.searchspring.io/api/search/search.json";
+
   return (
     <div className="App">
       <h1>Searchspring fashion</h1>
-      <label htmlFor="">Searchspring fashion</label>
-      <input type="text" placeholder="Search..." />
-      <button>Search</button>
+      <form action="">
+        <label htmlFor="label" className="label">
+          Searchspring fashion:
+        </label>
+        <input type="text" placeholder="Search for Brand, Color, Size..." />
+        <button>Search</button>
+      </form>
     </div>
   );
 }
