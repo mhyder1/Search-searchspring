efa1967e3e7ebd9f933eb768009392dc10f4bfe9
Mayen Akpan <neroxma@yahoo.com>
2021-07-18 07:25:47 -0700

    Update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import "./App.css";
+import React, { useState, useEffect } from "react";
 
 function App() {
   const SEARCH_API_URL =
     "https://scmq7n.a.searchspring.io/api/search/search.json?q=dress&resultsFormat=native&siteId=scmq7n";
   //[siteId].a.searchspring.io/api/search/search.json?q=dress&resultsFormat=native&siteId=[siteId]
 
-  https: return (
+  const [search, setSearch] = useState("");
+
+  const getData = () => {
+    fetch(SEARCH_API_URL)
+      .then((res) => res.json())
+      .then((data) => setSearch(data));
+  };
+
+  return (
     <div className="App">
       <div className="container">
         <h1 className="title">Searchspring fashion</h1>
@@ -15,6 +24,7 @@ function App() {
           </label>
           <input
             type="text"
+            name="text"
             placeholder="Search for Brand, Color, Size..."
             className="input"
           />
