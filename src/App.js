46914ad9a9ef639715aa3a1e1ed0829e511e4946
Mayen Akpan <neroxma@yahoo.com>
2021-07-19 21:34:07 -0700

    Updated

--- src/App.js
+++ src/App.js
@@ -3,22 +3,34 @@ import React, { useState, useEffect } from "react";
 
 function App() {
   const SEARCH_API_URL =
-    "https://scmq7n.a.searchspring.io/api/search/search.json?q=dress&resultsFormat=native&siteId=scmq7n";
+    "https://scmq7n.a.searchspring.io/api/search/search.json?resultsFormat=native&siteId=scmq7n&q=";
   //[siteId].a.searchspring.io/api/search/search.json?q=dress&resultsFormat=native&siteId=[siteId]
 
   const [search, setSearch] = useState("");
+  const [results, setResult] = useState(null);
 
-  const getData = () => {
-    fetch(SEARCH_API_URL)
+  const getData = (e) => {
+    e.preventDefault();
+    console.log("In GET data");
+    const url = SEARCH_API_URL + search;
+    fetch(url)
       .then((res) => res.json())
-      .then((data) => setSearch(data));
+      .then((data) => {
+        console.log(data);
+        console.log(data.results);
+        setResult(data.results);
+      });
+  };
+
+  const handleChange = (e) => {
+    setSearch(e.target.value);
   };
 
   return (
     <div className="App">
       <div className="container">
         <h1 className="title">Searchspring fashion</h1>
-        <form action="" className="form">
+        <form action="" className="form" onSubmit={getData}>
           <label htmlFor="label" className="label">
             Searchspring fashion:
           </label>
@@ -27,11 +39,25 @@ function App() {
             name="text"
             placeholder="Search for Brand, Color, Size..."
             className="input"
+            onChange={(search) => handleChange(search)}
+            search={search}
           />
           <button type="submit" className="button">
             Search
           </button>
         </form>
+        <div>
+          <div id="resultsDisplay">
+            {/* {results != null ? results.length : "no"} */}
+            {results != null ? (
+              results.map((item, i) => {
+                return <p>{item.name}</p>;
+              })
+            ) : (
+              <div>Search for something </div>
+            )}
+          </div>
+        </div>
       </div>
     </div>
   );
