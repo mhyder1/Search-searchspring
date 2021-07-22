fb9c1f800a434c9808fa88cf53fa43d3b0ddd04c
Mayen Akpan <neroxma@yahoo.com>
2021-07-21 21:29:52 -0700

    Added more code

--- src/App.js
+++ src/App.js
@@ -8,17 +8,37 @@ function App() {
 
   const [search, setSearch] = useState("");
   const [results, setResult] = useState(null);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [nextPage, setNextPage] = useState(2);
+  const [totalPages, setTotalPages] = useState(0);
+  const [previousPage, setPreviousPage] = useState(0);
+  const [pages, setPages] = useState([]);
+
+  const initalisePagesArray = (total) => {
+    console.log("seting pages");
+    console.log(total);
+    for (var i = 1; i < total; i++) {
+      setPages((pages) => [...pages, i]);
+      console.log(i);
+    }
+    console.log(pages);
+  };
 
   const getData = (e) => {
     e.preventDefault();
     console.log("In GET data");
-    const url = SEARCH_API_URL + search;
+    const url = SEARCH_API_URL + search + "&page=" + currentPage;
     fetch(url)
       .then((res) => res.json())
       .then((data) => {
-        console.log(data);
-        console.log(data.results);
         setResult(data.results);
+        const pagination = data.pagination;
+        setCurrentPage(pagination.currentPage);
+        setNextPage(pagination.nextPage);
+        setTotalPages(pagination.totalPages);
+        setPreviousPage(pagination.previousPage);
+        initalisePagesArray(pagination.totalPages);
+        console.log(data);
       });
   };
 
@@ -27,6 +47,12 @@ function App() {
   };
 
   const handlePageNumbers = () => {};
+  const handleButtonClick = (buttonNumber) => {
+    //update current page
+    //update previous page
+    //update nextPage
+    //call getData again
+  };
 
   return (
     <div className="App">
@@ -49,6 +75,21 @@ function App() {
           </button>
         </form>
         <div>
+          <div className="pagination">
+            {previousPage > 0 ? <div> Prev </div> : <div></div>}
+            {pages.length > 0 ? (
+              pages.map((pageNumber) => {
+                return (
+                  <button id={pageNumber} onClick={handleButtonClick}>
+                    {pageNumber}
+                  </button>
+                );
+              })
+            ) : (
+              <div> No pages </div>
+            )}
+            {nextPage > 0 ? <div> Next </div> : <div></div>}
+          </div>
           <div className="resultsDisplay">
             {/* {results != null ? results.length : "no"} */}
             {results != null ? (
@@ -60,15 +101,23 @@ function App() {
                   <div className="itemName">
                     <p>{item.name}</p>
                   </div>
-                  <div className="itemPrice">
-                    {/* {item.msrp && item.msrp * 1 > item.price * 1 
-                      ? item.price
-                      : item.msrp} */}
-                    <p>${item.price}</p>
-                    <p style={{ textDecorationLine: "line-through" }}>
-                      ${item.msrp}
-                    </p>
-                  </div>
+                  {item.msrp != null && item.msrp > item.price ? (
+                    <div className="itemPrice">
+                      <p id="itemPriceP">${item.price}</p>
+                      <p
+                        style={{
+                          textDecorationLine: "line-through",
+                          display: "inline",
+                        }}
+                      >
+                        ${item.msrp}
+                      </p>
+                    </div>
+                  ) : (
+                    <div className="itemPrice">
+                      <p>${item.price}</p>
+                    </div>
+                  )}
                 </div>
 
                 // <img src={item.thumbnailImageUrl} alt="image" />;
@@ -77,7 +126,7 @@ function App() {
               <div>Search for something </div>
             )}
           </div>
-          {results.pagination}
+          {/* {results.pagination} */}
         </div>
       </div>
     </div>
